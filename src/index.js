a502af8875d17e0bcb7f1820e426428bf21c82b1
tynidev <nichols.tyler@gmail.com>
2023-02-09 22:54:26 -0700

    Minor modifications

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
+import './css/root.css';
 import reportWebVitals from './reportWebVitals';
 
 import WsSubscribers from './ws_subscribers'

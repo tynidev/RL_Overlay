060dbd0a49ea5aeb4c0c63948c2fb216bc3d6c53
tynidev <nichols.tyler@gmail.com>
2023-02-22 11:21:55 -0700

    Refactoring of PostGameStats

--- src/components/PostGameStats.tsx
+++ src/components/PostGameStats.tsx
@@ -55,12 +55,21 @@ interface Stats {
   demos: TeamCount;
 }
 
-function fillTeams(
+interface TeamTable {
+  mvpHeader:Array<boolean>;
+  nameHeader:Array<string>; 
+  statsRows:Array<Array<string>>;
+}
+
+function FillTeams_AggregateStats_GetMVP(
   teams: Team[],
   left_orig: Player[],
   right_orig: Player[]
-): [Player[], Player[], Stats, boolean[]] {
-  let mvp = [false, false, false, false, false, false];
+): [TeamTable, TeamTable, Stats] {
+
+  let leftTable:TeamTable = {mvpHeader:[], nameHeader:[], statsRows:[[],[],[],[],[],[]]};
+  let rightTable:TeamTable = {mvpHeader:[], nameHeader:[], statsRows:[[],[],[],[],[],[]]};
+
   let mvp_max = 0;
   let mvp_idx = 0;
 
@@ -73,48 +82,111 @@ function fillTeams(
     demos: { left: 0, right: 0 },
   };
 
-  const left: Player[] = [];
   for (let i = 0; i < 3; i++) {
     if (i < left_orig.length) {
+      // Fill Team with player
       let p = left_orig[i];
-      left.push(left_orig[i]);
+      leftTable.mvpHeader.push(false);
+      leftTable.nameHeader.push(p.name);
+      leftTable.statsRows[0].push(p.score.toString());
+      leftTable.statsRows[1].push(p.goals.toString());
+      leftTable.statsRows[2].push(p.assists.toString());
+      leftTable.statsRows[3].push(p.shots.toString());
+      leftTable.statsRows[4].push(p.saves.toString());
+      leftTable.statsRows[5].push(p.demos.toString());
+
+      // Aggregate Stats
       stats.score.left += p.score;
       stats.goals.left += p.goals;
       stats.assists.left += p.assists;
       stats.shots.left += p.shots;
       stats.saves.left += p.saves;
       stats.demos.left += p.demos;
+
+      // Calculate MVP
       if (teams[0].score >= teams[1].score && p.score >= mvp_max) {
         mvp_idx = i;
         mvp_max = p.score;
       }
     } else {
-      left.push(NewPlayer());
+      // Fill Team empty player
+      leftTable.mvpHeader.push(false);
+      leftTable.nameHeader.push('');
+      leftTable.statsRows[0].push('');
+      leftTable.statsRows[1].push('');
+      leftTable.statsRows[2].push('');
+      leftTable.statsRows[3].push('');
+      leftTable.statsRows[4].push('');
+      leftTable.statsRows[5].push('');
     }
   }
-  const right: Player[] = [];
+
   for (let i = 0; i < 3; i++) {
     if (i < right_orig.length) {
+      // Fill Team with player
       let p = right_orig[i];
-      right.push(right_orig[i]);
+      rightTable.mvpHeader.push(false);
+      rightTable.nameHeader.push(p.name);
+      rightTable.statsRows[0].push(p.score.toString());
+      rightTable.statsRows[1].push(p.goals.toString());
+      rightTable.statsRows[2].push(p.assists.toString());
+      rightTable.statsRows[3].push(p.shots.toString());
+      rightTable.statsRows[4].push(p.saves.toString());
+      rightTable.statsRows[5].push(p.demos.toString());
+
+      // Aggregate Stats
       stats.score.right += p.score;
       stats.goals.right += p.goals;
       stats.assists.right += p.assists;
       stats.shots.right += p.shots;
       stats.saves.right += p.saves;
       stats.demos.right += p.demos;
+
+      // Calculate MVP
       if (teams[1].score >= teams[0].score && p.score >= mvp_max) {
         mvp_idx = i + 3;
         mvp_max = p.score;
       }
     } else {
-      right.push(NewPlayer());
+      // Fill Team empty player
+      rightTable.mvpHeader.push(false);
+      rightTable.nameHeader.push('');
+      rightTable.statsRows[0].push('');
+      rightTable.statsRows[1].push('');
+      rightTable.statsRows[2].push('');
+      rightTable.statsRows[3].push('');
+      rightTable.statsRows[4].push('');
+      rightTable.statsRows[5].push('');
     }
   }
 
-  mvp[mvp_idx] = true;
+  if(mvp_idx < 3)
+    leftTable.mvpHeader[mvp_idx] = true;
+  else
+    rightTable.mvpHeader[mvp_idx - 3] = true;
+  
+  const swapElement = (array:Array<any>, indexA:number, indexB:number) => {
+    var tmp = array[indexA];
+    array[indexA] = array[indexB];
+    array[indexB] = tmp;
+  }
+  
+  [leftTable.mvpHeader, leftTable.nameHeader].forEach(element => {
+    swapElement(element, 0, 1);
+    swapElement(element, 0, 2);
+  });
+  leftTable.statsRows.forEach(element => {
+    swapElement(element, 0, 1);
+    swapElement(element, 0, 2);
+  });
 
-  return [left, right, stats, mvp];
+  swapElement(rightTable.mvpHeader, 0, 1);
+  swapElement(rightTable.nameHeader, 0, 1);
+  rightTable.statsRows.forEach(element => {
+    swapElement(element, 0, 1);
+  });
+
+  return [leftTable, rightTable, stats];
 }
 
 function calcPct({ left, right }: TeamCount): number {
@@ -124,19 +196,19 @@ function calcPct({ left, right }: TeamCount): number {
 
 const PostGameStatsCore: FC<PostGameProps> = (props) => {
  
-  const [left_team, right_team, stats, mvp] = fillTeams(
+  const [leftTable, rightTable, stats] = FillTeams_AggregateStats_GetMVP(
     props.teams,
     props.left,
     props.right
   );
 
   const leftStatSliderWidth = {
-    score: calcPct(stats.score),
-    goals: calcPct(stats.goals),
-    assists: calcPct(stats.assists),
-    shots: calcPct(stats.shots),
-    saves: calcPct(stats.saves),
-    demos: calcPct(stats.demos),
+    'score': calcPct(stats.score),
+    'goals': calcPct(stats.goals),
+    'assists': calcPct(stats.assists),
+    'shots': calcPct(stats.shots),
+    'saves': calcPct(stats.saves),
+    'demos': calcPct(stats.demos),
   };
 
   let series_txt = '';
@@ -158,6 +230,42 @@ const PostGameStatsCore: FC<PostGameProps> = (props) => {
         ? formText(props.series.teams[1].name, right_won, left_won)
         : `series tied ${left_won}-${right_won}`;
   }
+
+  const TeamStatTable = (className:any, team:TeamTable):JSX.Element => {
+    return (<table className={className}>
+    <thead>
+      <tr className="mvp-row">
+        {team.mvpHeader.map((value, index) => {
+          return <th><img className="mvp" src={mvp_svg} alt="" style={{ visibility: value ? 'visible' : 'hidden' }} key={index}/></th>
+        })}
+      </tr>
+      <tr className="name">
+        {team.nameHeader.map((value, index) => {
+          return <th key={index}>{value}</th>
+        })}
+      </tr>
+    </thead>
+    <tbody>
+      {team.statsRows.map((stats, statIdx):JSX.Element => {
+        return <tr key={statIdx}>{stats.map((playerStat, playerStatIdx):JSX.Element =>{
+          return <td key={playerStatIdx}>{playerStat}</td>;
+        })}</tr>
+      })}
+    </tbody>
+  </table>);
+  };
+
+  let sliders:JSX.Element[] = [];
+  Object.keys(leftStatSliderWidth).forEach((sliderStat, idx) => {
+    let value = leftStatSliderWidth[sliderStat as keyof typeof leftStatSliderWidth];
+    sliders.push(<div className="stat-slider" key={idx}>
+      <div className="stat-slider-name">{sliderStat}</div>
+      <div className="stat-slider-box">
+        <div className="stat-slider-left" style={{ width: value }} />
+        <div className="stat-slider-right"style={{ width: 360 - 10 - value, left: value + 5 }} />
+      </div>
+    </div>);
+  });
 
   return (
     <div
@@ -181,242 +289,14 @@ const PostGameStatsCore: FC<PostGameProps> = (props) => {
 
       <div className="bottom-postgame-darken"></div>
 
-      <table className="left-team-stats">
-        <thead>
-          <tr className="mvp-row">
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[2] ? 'visible' : 'hidden' }}
-              />
-            </th>
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[0] ? 'visible' : 'hidden' }}
-              />
-            </th>
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[1] ? 'visible' : 'hidden' }}
-              />
-            </th>
-          </tr>
-          <tr className="name">
-            <th>{left_team[1].name}</th>
-            <th>{left_team[0].name}</th>
-            <th>{left_team[2].name}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="score">
-            <td>{left_team[1].name !== '' ? left_team[1].score : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].score : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].score : ''}</td>
-          </tr>
-          <tr className="goals">
-            <td>{left_team[1].name !== '' ? left_team[1].goals : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].goals : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].goals : ''}</td>
-          </tr>
-          <tr className="assists">
-            <td>{left_team[1].name !== '' ? left_team[1].assists : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].assists : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].assists : ''}</td>
-          </tr>
-          <tr className="shots">
-            <td>{left_team[1].name !== '' ? left_team[1].shots : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].shots : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].shots : ''}</td>
-          </tr>
-          <tr className="saves">
-            <td>{left_team[1].name !== '' ? left_team[1].saves : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].saves : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].saves : ''}</td>
-          </tr>
-          <tr className="demos">
-            <td>{left_team[1].name !== '' ? left_team[1].demos : ''}</td>
-            <td>{left_team[0].name !== '' ? left_team[0].demos : ''}</td>
-            <td>{left_team[2].name !== '' ? left_team[2].demos : ''}</td>
-          </tr>
-        </tbody>
-      </table>
+      {TeamStatTable("left-team-stats", leftTable)}
 
       <div className="stat-sliders">
-        <div className="stat-slider" style={{ height: '80px' }}>
-          <div className="stat-slider-name" style={{ lineHeight: '70px' }}>
-            score
-          </div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.score }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.score,
-                left: leftStatSliderWidth.score + 5,
-              }}
-            />
-          </div>
-        </div>
-        <div className="stat-slider">
-          <div className="stat-slider-name">goals</div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.goals }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.goals,
-                left: leftStatSliderWidth.goals + 5,
-              }}
-            />
-          </div>
-        </div>
-        <div className="stat-slider">
-          <div className="stat-slider-name">assists</div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.assists }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.assists,
-                left: leftStatSliderWidth.assists + 5,
-              }}
-            />
-          </div>
-        </div>
-        <div className="stat-slider">
-          <div className="stat-slider-name">shots</div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.shots }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.shots,
-                left: leftStatSliderWidth.shots + 5,
-              }}
-            />
-          </div>
-        </div>
-        <div className="stat-slider">
-          <div className="stat-slider-name">saves</div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.saves }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.saves,
-                left: leftStatSliderWidth.saves + 5,
-              }}
-            />
-          </div>
-        </div>
-        <div className="stat-slider">
-          <div className="stat-slider-name">demos</div>
-          <div className="stat-slider-box">
-            <div
-              className="stat-slider-left"
-              style={{ width: leftStatSliderWidth.demos }}
-            />{' '}
-            <div
-              className="stat-slider-right"
-              style={{
-                width: 360 - 10 - leftStatSliderWidth.demos,
-                left: leftStatSliderWidth.demos + 5,
-              }}
-            />
-          </div>
-        </div>
+        {sliders}
       </div>
 
-      <table className="right-team-stats">
-        <thead>
-          <tr className="mvp-row">
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[5] ? 'visible' : 'hidden' }}
-              />
-            </th>
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[3] ? 'visible' : 'hidden' }}
-              />
-            </th>
-            <th>
-              <img
-                className="mvp"
-                src={mvp_svg}
-                alt=""
-                style={{ visibility: mvp[4] ? 'visible' : 'hidden' }}
-              />
-            </th>
-          </tr>
-          <tr className="name">
-            <th>{right_team[2].name}</th>
-            <th>{right_team[0].name}</th>
-            <th>{right_team[1].name}</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr className="score">
-            <td>{right_team[2].name !== '' ? right_team[2].score : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].score : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].score : ''}</td>
-          </tr>
-          <tr className="goals">
-            <td>{right_team[2].name !== '' ? right_team[2].goals : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].goals : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].goals : ''}</td>
-          </tr>
-          <tr className="assists">
-            <td>{right_team[2].name !== '' ? right_team[2].assists : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].assists : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].assists : ''}</td>
-          </tr>
-          <tr className="shots">
-            <td>{right_team[2].name !== '' ? right_team[2].shots : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].shots : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].shots : ''}</td>
-          </tr>
-          <tr className="saves">
-            <td>{right_team[2].name !== '' ? right_team[2].saves : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].saves : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].saves : ''}</td>
-          </tr>
-          <tr className="demos">
-            <td>{right_team[2].name !== '' ? right_team[2].demos : ''}</td>
-            <td>{right_team[0].name !== '' ? right_team[0].demos : ''}</td>
-            <td>{right_team[1].name !== '' ? right_team[1].demos : ''}</td>
-          </tr>
-        </tbody>
-      </table>
+      {TeamStatTable("right-team-stats", rightTable)}
+
       <div className="left-player-names-underline"></div>
       <div className="right-player-names-underline"></div>
     </div>
@@ -426,20 +306,21 @@ const PostGameStatsCore: FC<PostGameProps> = (props) => {
 function ShouldUpdate(prevProps:PostGameProps, nextProps:PostGameProps): boolean{
   return!(
     prevProps.display !== nextProps.display ||
+    // Team Scores for current game
     prevProps.teams.length !== nextProps.teams.length ||
     prevProps.teams[0].score !== nextProps.teams[0].score ||
     prevProps.teams[1].score !== nextProps.teams[1].score ||
-    !areEqual(prevProps.left, nextProps.left, arePlayersEqual) ||
-    !areEqual(prevProps.right, nextProps.right, arePlayersEqual) ||
+    // Series Info
     prevProps.series.length !== nextProps.series.length ||
     prevProps.series.series_txt !== nextProps.series.series_txt ||
     prevProps.series.teams.length !== nextProps.series.teams.length ||
     prevProps.series.teams[0].name !== nextProps.series.teams[0].name ||
     prevProps.series.teams[1].name !== nextProps.series.teams[1].name ||
-    prevProps.series.teams[0].matches_won !==
-      nextProps.series.teams[0].matches_won ||
-    prevProps.series.teams[1].matches_won !==
-      nextProps.series.teams[1].matches_won);
+    prevProps.series.teams[0].matches_won !== nextProps.series.teams[0].matches_won ||
+    prevProps.series.teams[1].matches_won !== nextProps.series.teams[1].matches_won ||
+    // Left and Right team members
+    !areEqual(prevProps.left, nextProps.left, arePlayersEqual) ||
+    !areEqual(prevProps.right, nextProps.right, arePlayersEqual));
 }
 export const PostGameStats = React.memo(
   PostGameStatsCore,
